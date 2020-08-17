ede881bdd511398a5636e693f430feb68fc20c0f
akimzzy <a.akeem@yahoo.com>
2020-08-17 13:07:05 +0100

    Updates

---
+++ precache-manifest.83a86a4d8ce8edcbacc987e83d8f250c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20053f10c91954b2434b52568abef468",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "ee5e29766b6d50ddc311",
+    "url": "/expense-tracker/static/css/main.49ce3ecf.chunk.css"
+  },
+  {
+    "revision": "8b260261b4aeb3cbb1ec",
+    "url": "/expense-tracker/static/js/2.70fe2c47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.70fe2c47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee5e29766b6d50ddc311",
+    "url": "/expense-tracker/static/js/main.b4ec8cad.chunk.js"
+  },
+  {
+    "revision": "3c5857f97e6b39633045",
+    "url": "/expense-tracker/static/js/runtime-main.932dea0c.js"
+  }
+]);
